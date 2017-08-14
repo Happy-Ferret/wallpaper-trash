1b0d256ec751c447d3d49e7eb2fa822a875705c6
Happy-Ferret <MarkBauer@gmx.ch>
2017-08-14 13:33:39 +0200

    Removed some more onboarding@mozilla code. Added event listener for file picker.
    TODO:
    
    - Implement "setWallpaper" method,
    setting shell.wallpaper.url and shell.wallpaper.type.
    
    - Implement "getWallpaper" method, reading
    from shell.wallpaper pref branch and
    returning the result as a struct to renderWallpaper.
    
    - Close customization overlay upon successful wallpaper change.

--- chrome/content/wallpaper.js
+++ chrome/content/wallpaper.js
@@ -25,199 +25,6 @@ const BRAND_SHORT_NAME = Services.strings
   .GetStringFromName("brandShortName");
 
 /**
- * Add any number of tours, following the format
- * {
- *   // The unique tour id
- *   id: "wallpaper-tour-addons",
- *   // The string id of tour name which would be displayed on the navigation bar
- *   tourNameId: "wallpaper.tour-addon",
- *   // The method returing strings used on tour notification
- *   getNotificationStrings(bundle):
- *     - title: // The string of tour notification title
- *     - message: // The string of tour notification message
- *     - button: // The string of tour notification action button title
- *   // Return a div appended with elements for this tours.
- *   // Each tour should contain the following 3 sections in the div:
- *   // .wallpaper-tour-description, .wallpaper-tour-content, .wallpaper-tour-button-container.
- *   // Add wallpaper-no-button css class in the div if this tour does not need a button container.
- *   // If there was a .wallpaper-tour-action-button present and was clicked, tour would be marked as completed.
- *   getPage() {},
- * },
- **/
-var wallpaperTours = [
-  {
-    id: "wallpaper-tour-private-browsing",
-    tourNameId: "wallpaper.tour-private-browsing",
-    getNotificationStrings(bundle) {
-      return {
-        title: bundle.GetStringFromName("wallpaper.notification.wallpaper-tour-private-browsing.title"),
-        message: bundle.GetStringFromName("wallpaper.notification.wallpaper-tour-private-browsing.message"),
-        button: bundle.GetStringFromName("wallpaper.button.learnMore"),
-      };
-    },
-    getPage(win) {
-      let div = win.document.createElement("div");
-      div.innerHTML = `
-        <section class="wallpaper-tour-description">
-          <h1 data-l10n-id="wallpaper.tour-private-browsing.title"></h1>
-          <p data-l10n-id="wallpaper.tour-private-browsing.description"></p>
-        </section>
-        <section class="wallpaper-tour-content">
-          <img src="resource://wallpaper/img/figure_private.svg" />
-        </section>
-        <aside class="wallpaper-tour-button-container">
-          <button id="wallpaper-tour-private-browsing-button" class="wallpaper-tour-action-button" data-l10n-id="wallpaper.tour-private-browsing.button"></button>
-        </aside>
-      `;
-      return div;
-    },
-  },
-  {
-    id: "wallpaper-tour-addons",
-    tourNameId: "wallpaper.tour-addons",
-    getNotificationStrings(bundle) {
-      return {
-        title: bundle.GetStringFromName("wallpaper.notification.wallpaper-tour-addons.title"),
-        message: bundle.formatStringFromName("wallpaper.notification.wallpaper-tour-addons.message", [BRAND_SHORT_NAME], 1),
-        button: bundle.GetStringFromName("wallpaper.button.learnMore"),
-      };
-    },
-    getPage(win) {
-      let div = win.document.createElement("div");
-      div.innerHTML = `
-        <section class="wallpaper-tour-description">
-          <h1 data-l10n-id="wallpaper.tour-addons.title"></h1>
-          <p data-l10n-id="wallpaper.tour-addons.description"></p>
-        </section>
-        <section class="wallpaper-tour-content">
-          <img src="resource://wallpaper/img/figure_addons.svg" />
-        </section>
-        <aside class="wallpaper-tour-button-container">
-          <button id="wallpaper-tour-addons-button" class="wallpaper-tour-action-button" data-l10n-id="wallpaper.tour-addons.button"></button>
-        </aside>
-      `;
-      return div;
-    },
-  },
-  {
-    id: "wallpaper-tour-customize",
-    tourNameId: "wallpaper.tour-customize",
-    getNotificationStrings(bundle) {
-      return {
-        title: bundle.GetStringFromName("wallpaper.notification.wallpaper-tour-customize.title"),
-        message: bundle.formatStringFromName("wallpaper.notification.wallpaper-tour-customize.message", [BRAND_SHORT_NAME], 1),
-        button: bundle.GetStringFromName("wallpaper.button.learnMore"),
-      };
-    },
-    getPage(win) {
-      let div = win.document.createElement("div");
-      div.innerHTML = `
-        <section class="wallpaper-tour-description">
-          <h1 data-l10n-id="wallpaper.tour-customize.title"></h1>
-          <p data-l10n-id="wallpaper.tour-customize.description"></p>
-        </section>
-        <section class="wallpaper-tour-content">
-          <img src="resource://wallpaper/img/figure_customize.svg" />
-        </section>
-        <aside class="wallpaper-tour-button-container">
-          <button id="wallpaper-tour-customize-button" class="wallpaper-tour-action-button" data-l10n-id="wallpaper.tour-customize.button"></button>
-        </aside>
-      `;
-      return div;
-    },
-  },
-  {
-    id: "wallpaper-tour-search",
-    tourNameId: "wallpaper.tour-search",
-    getNotificationStrings(bundle) {
-      return {
-        title: bundle.GetStringFromName("wallpaper.notification.wallpaper-tour-search.title"),
-        message: bundle.GetStringFromName("wallpaper.notification.wallpaper-tour-search.message"),
-        button: bundle.GetStringFromName("wallpaper.button.learnMore"),
-      };
-    },
-    getPage(win) {
-      let div = win.document.createElement("div");
-      div.innerHTML = `
-        <section class="wallpaper-tour-description">
-          <h1 data-l10n-id="wallpaper.tour-search.title"></h1>
-          <p data-l10n-id="wallpaper.tour-search.description"></p>
-        </section>
-        <section class="wallpaper-tour-content">
-          <img src="resource://wallpaper/img/figure_search.svg" />
-        </section>
-        <aside class="wallpaper-tour-button-container">
-          <button id="wallpaper-tour-search-button" class="wallpaper-tour-action-button" data-l10n-id="wallpaper.tour-search.button"></button>
-        </aside>
-      `;
-      return div;
-    },
-  },
-  {
-    id: "wallpaper-tour-default-browser",
-    tourNameId: "wallpaper.tour-default-browser",
-    getNotificationStrings(bundle) {
-      return {
-        title: bundle.formatStringFromName("wallpaper.notification.wallpaper-tour-default-browser.title", [BRAND_SHORT_NAME], 1),
-        message: bundle.formatStringFromName("wallpaper.notification.wallpaper-tour-default-browser.message", [BRAND_SHORT_NAME], 1),
-        button: bundle.GetStringFromName("wallpaper.button.learnMore"),
-      };
-    },
-    getPage(win) {
-      let div = win.document.createElement("div");
-      let defaultBrowserButtonId = win.matchMedia("(-moz-os-version: windows-win7)").matches ?
-        "wallpaper.tour-default-browser.win7.button" : "wallpaper.tour-default-browser.button";
-      div.innerHTML = `
-        <section class="wallpaper-tour-description">
-          <h1 data-l10n-id="wallpaper.tour-default-browser.title"></h1>
-          <p data-l10n-id="wallpaper.tour-default-browser.description"></p>
-        </section>
-        <section class="wallpaper-tour-content">
-          <img src="resource://wallpaper/img/figure_default.svg" />
-        </section>
-        <aside class="wallpaper-tour-button-container">
-          <button id="wallpaper-tour-default-browser-button" class="wallpaper-tour-action-button" data-l10n-id="${defaultBrowserButtonId}"></button>
-        </aside>
-      `;
-      return div;
-    },
-  },
-  {
-    id: "wallpaper-tour-sync",
-    tourNameId: "wallpaper.tour-sync",
-    getNotificationStrings(bundle) {
-      return {
-        title: bundle.GetStringFromName("wallpaper.notification.wallpaper-tour-sync.title"),
-        message: bundle.GetStringFromName("wallpaper.notification.wallpaper-tour-sync.message"),
-        button: bundle.GetStringFromName("wallpaper.button.learnMore"),
-      };
-    },
-    getPage(win, bundle) {
-      let div = win.document.createElement("div");
-      div.classList.add("wallpaper-no-button");
-      div.innerHTML = `
-        <section class="wallpaper-tour-description">
-          <h1 data-l10n-id="wallpaper.tour-sync.title"></h1>
-          <p data-l10n-id="wallpaper.tour-sync.description"></p>
-        </section>
-        <section class="wallpaper-tour-content">
-          <form>
-            <h3 data-l10n-id="wallpaper.tour-sync.form.title"></h3>
-            <p data-l10n-id="wallpaper.tour-sync.form.description"></p>
-            <input id="wallpaper-tour-sync-email-input" type="text"></input><br />
-            <button id="wallpaper-tour-sync-button" class="wallpaper-tour-action-button" data-l10n-id="wallpaper.tour-sync.button"></button>
-          </form>
-          <img src="resource://wallpaper/img/figure_sync.svg" />
-        </section>
-      `;
-      div.querySelector("#wallpaper-tour-sync-email-input").placeholder =
-        bundle.GetStringFromName("wallpaper.tour-sync.email-input.placeholder");
-      return div;
-    },
-  },
-];
-
-/**
  * The script won't be initialized if we turned off wallpaper by
  * setting "browser.wallpaper.enabled" to false.
  */
@@ -231,11 +38,12 @@ class Wallpaper {
     this._tourItems = [];
     this._tourPages = [];
 
-    var nsIFilePicker = Ci.nsIFilePicker;
-    var fp = Cc["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-    fp.appendFilter("Animated", "*.webm");
-    fp.appendFilter("Still", "*.jpg");
-    fp.init(this._window, "Select a wallpaper", nsIFilePicker.modeOpen);
+    this.nsIFilePicker = Ci.nsIFilePicker;
+    this.fp = Cc["@mozilla.org/filepicker;1"].createInstance(this.nsIFilePicker);
+    
+    this.fp.appendFilter("Animated", "*.webm");
+    this.fp.appendFilter("Still", "*.jpg");
+    this.fp.init(this._window, "Select a wallpaper", this.nsIFilePicker.modeOpen);
 
     // we only support the new user tour at this moment
     if (Services.prefs.getStringPref("browser.wallpaper.tour-type", "update") !== "new") {
@@ -246,7 +54,7 @@ class Wallpaper {
     // no flash of style changes and no additional reflow.
     await this._loadCSS();
     this._bundle = Services.strings.createBundle(BUNDLE_URI);
-    let url = "file:///C:/Users/marku/Desktop/opera.webm";
+    let url = "resource://wallpaper/Opera.webm";
     let type = "animated";
 
     // let url = "file:///C:/Users/marku/Desktop/photo-1471068139873-46abd34a24b7.jpg";
@@ -276,7 +84,7 @@ class Wallpaper {
     // No any leak out there.
     this._window.addEventListener("unload", () => this.destroy());
 
-    this._window.document.getElementById("newtab-customize-wallpaper").addEventListener("click", fp.show);
+    this._window.document.getElementById("newtab-customize-wallpaper").addEventListener("click", this);
 
     this._initPrefObserver();
   }
@@ -292,14 +100,6 @@ class Wallpaper {
         this.destroy();
       }
     });
-    wallpaperTours.forEach(tour => {
-      let tourId = tour.id;
-      this._prefsObserved.set(`browser.wallpaper.tour.${tourId}.completed`, () => {
-      });
-    });
-    for (let [name, callback] of this._prefsObserved) {
-      Preferences.observe(name, callback);
-    }
   }
 
   _clearPrefObserver() {
@@ -311,8 +111,13 @@ class Wallpaper {
     }
   }
 
-    _openWallpaper() {
-    fp.show()
+    _selectWallpaper(evt) {
+    // evt.target.fp.show()
+    var rv = this.fp.show();
+    if (rv == this.nsIFilePicker.returnOK || rv == this.nsIFilePicker.returnReplace) {
+      var path = this.fp.file.path;
+      console.log(path);
+    }
   }
 
   /**
@@ -327,24 +132,9 @@ class Wallpaper {
 
   handleEvent(evt) {
     switch (evt.target.id) {
-      case "wallpaper-overlay-icon":
-      case "wallpaper-overlay-close-btn":
-      // If the clicking target is directly on the outer-most overlay,
-      // that means clicking outside the tour content area.
-      // Let's toggle the overlay.
-      case "wallpaper-overlay":
-        this.toggleOverlay();
+        case "newtab-customize-wallpaper":
+        this._selectWallpaper()
         break;
-
-      case "wallpaper-notification-action-btn":
-        let tourId = this._notificationBar.dataset.targetTourId;
-        this.toggleOverlay();
-        break;
-    }
-    let classList = evt.target.classList;
-    if (classList.contains("wallpaper-tour-item")) {
-    } else if (classList.contains("wallpaper-tour-action-button")) {
-      let activeItem = this._tourItems.find(item => item.classList.contains("wallpaper-active"));
     }
   }
 
@@ -373,7 +163,6 @@ class Wallpaper {
   }
 
   _renderWallpaper() {
-    let url = "file:///C:/Users/marku/Desktop/opera.webm"
     let div = this._window.document.createElement("div");
     div.id = "wallpaper-overlay";
     div.innerHTML = `
@@ -390,10 +179,10 @@ class Wallpaper {
     div.className = "newtab-customize-panel-item newtab-customize-blank";
     // let onclick = this._openWallpaper();
     div.innerHTML = `
-            <label for="wallpaper.wallpaper.title"></label>
+            <label for="newtab-customize-wallpaper"></label>
             `;
 
-    div.querySelector("label[for='wallpaper.wallpaper.title']").textContent =
+    div.querySelector("label[for='newtab-customize-wallpaper']").textContent =
       this._bundle.GetStringFromName("wallpaper.wallpaper.title");
     return div;
   }
